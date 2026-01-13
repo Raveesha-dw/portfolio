cd2c7cfae632422ea5a843f0e01809c861a4dac2
Raveesha Weerasinghe <136602532+Raveesha-dw@users.noreply.github.com>
2026-01-14 00:38:47 +0530

    Grid added to section two

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -10,7 +10,7 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "mx-auto grid max-w-7xl grid-cols-1 gap-4 md:auto-rows-[18rem] md:grid-cols-3",
+        "mx-auto grid max-w-[1400px] grid-cols-1 gap-5 md:auto-rows-[14em] md:grid-cols-6",
         className,
       )}
     >
@@ -23,9 +23,11 @@ export const BentoGridItem = ({
   className,
   title,
   description,
-  header,
-  icon,
   id,
+  img,  
+  imgClassName,
+  titleClassName,
+  spareImg,
 }: {
   className?: string;
   title?: string | React.ReactNode;
@@ -33,17 +35,37 @@ export const BentoGridItem = ({
   header?: React.ReactNode;
   icon?: React.ReactNode;
   id: number;
+  img?: string;
+  imgClassName?: string;
+  titleClassName?: string;
+  spareImg?: string;
 }) => {
   return (
     <div
       className={cn(
-        "group/bento shadow-input row-span-1 flex flex-col relative justify-between space-y-4 rounded-3xl border border-red-500 bg-white p-4 transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none",
+        "group/bento shadow-input row-span-1 flex flex-col relative justify-between space-y-4 rounded-3xl border bg-white p-4 transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none",
         className,
       )}
+      style={{
+       background: '#01052e',
+      backgroundColor: 'linear-gradient(90deg,rgba(1, 5, 46, 1) 0%, rgba(42, 60, 84, 1) 50%, rgba(108, 158, 204, 1) 100%)',
+      }}
     >
-      {header}
+
+      <div className={`${id === 6} && 'flex justify-center h-full'`}>
+        <div className="w-full h-full absolute">
+        {img && (
+          <img
+          src={img}
+          alt={img}
+          className={cn(imgClassName)} />
+        )}
+        </div>
+
+      </div>
+      {/* {header} */}
       <div className="transition duration-200 group-hover/bento:translate-x-2">
-        {icon}
+        {/* {icon} */}
         <div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
           {title}
         </div>
